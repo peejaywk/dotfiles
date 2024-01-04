451def611e3a95123b3ee6539e8b4b297ae8a992
peejaywk <peejaywk@gmail.com>
2024-01-04 09:37:20 +0000

    Added the ssh agent and removed the gnu agent

--- hosts/homer/default.nix
+++ hosts/homer/default.nix
@@ -104,13 +104,17 @@
     zip
   ];
 
+  programs = {
+    ssh.startAgent = true;
+  };
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
-  programs.gnupg.agent = {
-     enable = true;
-     enableSSHSupport = true;
-  };
+  #programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  #};
 
   # List services that you want to enable:
 
