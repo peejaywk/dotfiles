ea2101091997a35d119c2dd87dff6bdefecc037e
peejaywk <peejaywk@gmail.com>
2024-01-04 14:59:43 +0000

    Added more mount points

--- hosts/common/mounts.nix
+++ hosts/common/mounts.nix
@@ -1,15 +1,40 @@
 { config, pkgs, ...}:
-{
-  fileSystems."/home/peejaywk/shared" = {
-    device = "192.168.1.76:volume1/Shared";
-    fsType = "nfs";
-    options = [
+  let
+    mount-options = [
+      "credentials=/home/peejaywk/.config/smbUser,uid=1000,gid=100,nobrl"
       "x-systemd.before=local-fs.target"
       "x-systemd.automount"
       "soft"
     ];
-  };
+  in {    
+    environment.systemPackages = [ pkgs.cifs-utils ];
 
+    fileSystems."/home/peejaywk/Shared/arr" = {
+      device = "//192.168.1.76/Shared/arr";
+      fsType = "cifs";
+      options = mount-options;
+    };
+    fileSystems."/home/peejaywk/Shared/Data" = {
+      device = "//192.168.1.76/data";
+      fsType = "cifs";
+      options = mount-options;
+    };
+    fileSystems."/home/peejaywk/Shared/Homes" = {
+      device = "//192.168.1.76/homes";
+      fsType = "cifs";
+      options = mount-options;
+    };
+    fileSystems."/home/peejaywk/Shared/MyMusic" = {
+      device = "//192.168.1.76/MyMusic";
+      fsType = "cifs";
+      options = mount-options;
+    };
+    fileSystems."/home/peejaywk/Shared/Videos" = {
+      device = "//192.168.1.76/video";
+      fsType = "cifs";
+      options = mount-options;
+    };
+  
   services = {
     rpcbind.enable = true;
     nfs.server.enable = true;
