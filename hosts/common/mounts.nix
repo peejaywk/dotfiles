917a734428f80990ac0c0b8ee5104a19e9ff0420
peejaywk <peejaywk@gmail.com>
2024-03-01 13:05:33 +0000

    Update mount points

--- hosts/common/mounts.nix
+++ hosts/common/mounts.nix
@@ -10,36 +10,35 @@
     environment.systemPackages = [ pkgs.cifs-utils ];
 
     fileSystems."/home/peejaywk/Shared/arr" = {
-      device = "//192.168.1.76/Shared/arr";
+      device = "//192.168.30.10/Shared/arr";
       fsType = "cifs";
       options = mount-options;
     };
     fileSystems."/home/peejaywk/Shared/Data" = {
-      device = "//192.168.1.76/data";
+      device = "//192.168.30.10/data";
       fsType = "cifs";
       options = mount-options;
     };
     fileSystems."/home/peejaywk/Shared/Homes" = {
-      device = "//192.168.1.76/homes";
+      device = "//192.168.30.10/homes";
       fsType = "cifs";
       options = mount-options;
     };
     fileSystems."/home/peejaywk/Shared/MyMusic" = {
-      device = "//192.168.1.76/MyMusic";
+      device = "//192.168.30.10/MyMusic";
       fsType = "cifs";
       options = mount-options;
     };
     fileSystems."/home/peejaywk/Shared/Videos" = {
-      device = "//192.168.1.76/video";
+      device = "//192.168.30.10/video";
       fsType = "cifs";
       options = mount-options;
     };
     fileSystems."/home/peejaywk/Shared/Docker" = {
-      device = "//192.168.1.76/Docker";
+      device = "//192.168.30.10/Docker";
       fsType = "cifs";
       options = mount-options;
     };
-  
   services = {
     rpcbind.enable = true;
     nfs.server.enable = true;
