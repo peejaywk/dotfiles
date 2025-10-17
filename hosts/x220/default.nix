84adf2f75fc1d5097535b9d1c252cb678fa5b39f
peejaywk <peejaywk@gmail.com>
2025-10-17 17:31:31 +0100

    Updated laptop to Plasma 6 Desktop

--- hosts/x220/default.nix
+++ hosts/x220/default.nix
@@ -75,8 +75,8 @@
   services.xserver.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
+  services.xserver.displayManager.sddm.wayland.enable = true;
+  services.desktopManager.plasma6.enable = true;
 
   # Configure keymap in X11
   services.xserver = {
